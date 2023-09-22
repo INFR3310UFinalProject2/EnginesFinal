1e8064aac62d6aab44581b694ffb1b621391cea2
Gabe Kotton <52292616+FlamingoeZpZ@users.noreply.github.com>
2023-09-22 14:45:16 -0400

    Uploaf

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float moveSpeed;
+
+
+
+    private void WhyDoIDoThis()
+    {
+        
+    }
+}
